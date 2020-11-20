a4d4ec2f7335bb8879f51faeb06742ad364eef16
Deivison Cardoso <deivisondc@gmail.com>
2020-11-20 00:45:41 -0300

    add connections json

--- src/components/Connection/index.tsx
+++ src/components/Connection/index.tsx
@@ -1,34 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiLink } from 'react-icons/fi';
 
+import connections from '../../assets/connections.json';
+
 import Card from '../Card';
+import Link from '../Link';
 
 import {
   Container,
   ImageGroup,
+  MiniAvatar,
   Avatar,
-  CollapsedContainer,
-  CollapsedContainerContent,
+  ContentContainer,
+  Content,
+  SeeMore,
 } from './styles';
 
 const Connection: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
     <Card title="Conexões" icon={<FiLink />}>
       <Container>
-        <CollapsedContainer>
+        <ContentContainer>
           <ImageGroup>
-            <Avatar src="https://media-exp1.licdn.com/dms/image/C4E03AQEFVCRAPFpbUA/profile-displayphoto-shrink_800_800/0?e=1611187200&v=beta&t=C1hgMaT2sw0GI3zMlXBvG-n_U_r_vpFJyXn2WuxZjjA" />
-            <Avatar src="https://media-exp1.licdn.com/dms/image/C4E03AQGIUOHfzExl3g/profile-displayphoto-shrink_800_800/0?e=1611187200&v=beta&t=rLvyMgaNTjd3LfI8xInTuobhp5bNlfSpcNerJ4fQv4w" />
+            <MiniAvatar src={connections[0].avatar} />
+            <MiniAvatar src={connections[1].avatar} />
           </ImageGroup>
-          <CollapsedContainerContent>
+          <Content>
             <h3>Muitas conexões em comum</h3>
             <p>
               Você e Adaptabilidade conhecem Resiliência, Auto Conhecimento e
               muitos outros.
             </p>
-          </CollapsedContainerContent>
-        </CollapsedContainer>
+          </Content>
+        </ContentContainer>
+
+        <ContentContainer>
+          {collapsed &&
+            connections.map(connection => (
+              <Content className="vertical" key={connection.name}>
+                <Link flexColumn to={connection.link}>
+                  <Avatar src={connection.avatar} />
+                  <h3>{connection.name}</h3>
+                  <p>{connection.occupation}</p>
+                </Link>
+              </Content>
+            ))}
+        </ContentContainer>
       </Container>
+
+      <SeeMore onClick={() => setCollapsed(!collapsed)}>
+        Ver {collapsed ? 'menos' : 'mais'}...
+      </SeeMore>
     </Card>
   );
 };
--- src/components/Connection/styles.ts
+++ src/components/Connection/styles.ts
@@ -5,14 +5,44 @@ export const Container = styled.div`
   padding: 0 20px;
 `;
 
-export const CollapsedContainer = styled.div`
+export const ContentContainer = styled.div`
   display: flex;
+  overflow: auto;
+
+  ::-webkit-scrollbar {
+    height: 7px;
+  }
+
+  ::-webkit-scrollbar-track {
+    border-radius: 5px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background: #ccc;
+    border-radius: 5px;
+  }
 `;
 
-export const CollapsedContainerContent = styled.div`
+export const Content = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+
+  &.vertical {
+    flex: 1 0 175px;
+    justify-content: center;
+    align-items: center;
+    margin-top: 25px;
+    margin-bottom: 15px;
+
+    h3 {
+      margin-top: 10px;
+    }
+
+    p {
+      margin-top: 5px;
+    }
+  }
 
   h3 {
     font-size: 18px;
@@ -43,10 +73,27 @@ export const ImageGroup = styled.div`
   }
 `;
 
-export const Avatar = styled.img`
+export const MiniAvatar = styled.img`
   position: absolute;
   width: 40px;
   height: 40px;
   border-radius: 50%;
   border: 2px solid #fff;
+`;
+
+export const Avatar = styled.img`
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+`;
+
+export const SeeMore = styled.button`
+  width: 100%;
+  background: none;
+  border: 0;
+  font-size: 14px;
+  font-weight: bold;
+  padding: 10px;
+  border-top: 1px solid #ddd;
+  border-radius: 0 0 8px 8px;
 `;
